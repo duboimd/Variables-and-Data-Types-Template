9f4474ae0f134cbc07f04512a261293f2700d603
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-09-16 21:10:01 -0600

    Create Variables and Data Types.py

---
+++ Variables and Data Types.py
@@ -0,0 +1,55 @@
+# VARIABLES
+
+# 1. You must define the following variables:
+# a. An int called luckyNumber which contains your favourite number
+
+
+# b. A string called favouriteFood which contains your favourite food
+
+
+# c. A boolean called iCanSki and set it equal to either True or False (if you can ski)
+
+
+# d. A float called myFloat which contains the value 10.3
+
+
+# e. An int called myAge which contains your age
+
+
+# f. A string called favHobby which contains your favourite hobby
+
+
+
+
+# 2. Create a variable named favAnimal and set it equal to your favourite animal. Then print favAnimal to the console.
+
+
+
+
+# 3. Create a variable named favColour and set it equal to your favourite colour. Then print favColour to the console.
+
+
+
+
+
+# 4. Create a variable named favMovie and set it equal to your favourite movie! 
+# Then print favMovie to the console.
+
+
+
+
+
+# 5. Create a variable named evenNumber and set it equal to an even number. Then create another variable named oddNumber and set it equal to an odd number.
+
+
+
+
+# 6. Create a variable named iLikeSummer and set it equal to a boolean (either True or False).
+
+
+
+# 7. Create a variable named iLikeIceCream and set it equal to a boolean (either True or False).
+
+
+
+# 8. Create a variable named phoneNumber and set it equal to a random phone number.
