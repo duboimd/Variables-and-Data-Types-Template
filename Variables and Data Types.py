526c1ae1c269b2b9afe7451d25200910a4cdfcaf
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2025-02-06 10:06:50 -0700

    Update Variables and Data Types.py

--- Variables and Data Types.py
+++ Variables and Data Types.py
@@ -1,55 +1,54 @@
 # VARIABLES
 
+print("Question 1a:")
 # 1. You must define the following variables:
 # a. An int called luckyNumber which contains your favourite number
 
-
+print()
+print("Question 1a:")
 # b. A string called favouriteFood which contains your favourite food
 
-
+print()
+print("Question 1c:")
 # c. A boolean called iCanSki and set it equal to either True or False (if you can ski)
 
-
+print()
+print("Question 1d:")
 # d. A float called myFloat which contains the value 10.3
 
-
+print()
+print("Question 1e:")
 # e. An int called myAge which contains your age
 
-
+print()
+print("Question 1f:")
 # f. A string called favHobby which contains your favourite hobby
 
-
-
-
+print()
+print("Question 2:")
 # 2. Create a variable named favAnimal and set it equal to your favourite animal. Then print favAnimal to the console.
 
-
-
-
+print()
+print("Question 3:")
 # 3. Create a variable named favColour and set it equal to your favourite colour. Then print favColour to the console.
 
-
-
-
-
+print()
+print("Question 4:")
 # 4. Create a variable named favMovie and set it equal to your favourite movie! 
 # Then print favMovie to the console.
 
-
-
-
-
+print()
+print("Question 5:")
 # 5. Create a variable named evenNumber and set it equal to an even number. Then create another variable named oddNumber and set it equal to an odd number.
 
-
-
-
+print()
+print("Question 6:")
 # 6. Create a variable named iLikeSummer and set it equal to a boolean (either True or False).
 
-
-
+print()
+print("Question 7:")
 # 7. Create a variable named iLikeIceCream and set it equal to a boolean (either True or False).
 
-
-
+print()
+print("Question 8:")
 # 8. Create a variable named phoneNumber and set it equal to a random phone number.
